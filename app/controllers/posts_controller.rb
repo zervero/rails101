ed9d0162cd861d1eabad0374e1035a22afb46ab7
zengxiangrong <zengxiangrong5261435@gmail.com>
2016-12-23 17:37:16 +0800

    练习自己实作“编辑”和“删除”按钮这两个功能！

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
-  
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -19,6 +19,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+  def update
+   @post = Post.find(params[:id])
+   if @post.update(post_params)
+     redirect_to account_posts_path, notice: "Update Success"
+   else
+     render :edit
+   end
+ end
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to account_posts_path, alert: "Post deleted"
+    end
+  end
 
   private
 
